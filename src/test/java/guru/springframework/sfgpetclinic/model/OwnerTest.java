ced1cef0ece7640277f2a540e04242f52f4f394b
lentiummmx <lentium.mmx@gmail.com>
2021-04-26 13:23:57 -0500

    testing-j-junit5 File(s) committed to adding dependent assertions examples s5-49 (v0.1.2)

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1l, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("1231231234");
+
+        assertAll("Properties Test",
+            () -> assertAll("Person Properties",
+                    () -> assertEquals("Joee", owner.getFirstName(), "First Name Did not Match"),
+                    () -> assertEquals("Buck", owner.getLastName())
+                ),
+            () -> assertAll("Owner Properties",
+                () -> assertEquals("Key Westt", owner.getCity(), "City Did not Match"),
+                () -> assertEquals("1231231234", owner.getTelephone())
+            )
+        );
+    }
+}
